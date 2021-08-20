66f83b694608e90b20f44cb503be6f6e53ffd58c
Conespoxs <87117498+Neeacconl@users.noreply.github.com>
2021-08-20 12:51:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 class Config:
     BOT_TOKEN = os.environ.get("1902600829:AAEQ8Ttu4ZFAeAx_rxBhw24fyv_f2KwH-SU","")
     HEROKU_API_KEY = os.environ.get("cf70b19a-3a5b-4a21-a8a3-825c1e4a405a","")
-    AUTHORIZED_USERS = [int(user) for user in os.environ.get("AUTHORIZED_USERS").split(" ")]
+    AUTHORIZED_USERS = [int(user) for user in os.environ.get("1088599147").split(" ")]
     TG_CHARACTER_LIMIT = 4000 
     HEROKU_APP_NAME = os.environ.get("appnamersh","")
